b1065e39878ccdd5b00913a1bc71e57cedb9a405
jalolovts <jalolovtursunbek@mail.ru>
2024-02-29 12:25:30 +0500

    sinov3

---
+++ 1 2 3 KT amaliy 2 semestr/3 kt/5 dars.py
@@ -0,0 +1,22 @@
+'''n = int(input('n = '))
+#c = n % 2 == 0
+#print('n =',n,'-->',c)
+
+if :
+    print('n =',n,'-->','juft son')
+else:
+    print('n =',n,'-->','toq son')'''
+'''
+a = int(input('a = '))
+if a > 0:
+    print('a =',a,'-->','musbat son')
+else:
+    print('a =',a,'-->','manfiy son')'''
+
+a = int(input('a = '))
+b = int(input('b = '))
+if a > 0:
+    print('a =',a,'-->','musbat son')
+else:
+    print('a =',a,'-->','manfiy son')
+
