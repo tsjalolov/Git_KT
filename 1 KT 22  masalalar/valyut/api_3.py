50498130e68d9a6ab9449a4065363b9dca216e57
tsjalolov <tursunbekjalolov@gmail.com>
2024-03-11 14:12:27 +0500

    aw pycharm1

--- 1 KT 22  masalalar/valyut/api_3.py
+++ 1 KT 22  masalalar/valyut/api_3.py
@@ -1,2 +1,12 @@
-print('salom')
+
 import requests
+json = requests.get(f'https://www.cbr-xml-daily.ru/daily_json.js').json()
+
+print('1-->', json)
+print('2-->', json.keys())
+print('3-->', json['Date'])
+print('4-->', json['Valute'])
+print('5-->', json['Valute'].keys())
+print('6-->', json['Valute']['UZS']['Name'])
+for i in json['Valute']:
+    print(i, json['Valute'][i]['Name'])
