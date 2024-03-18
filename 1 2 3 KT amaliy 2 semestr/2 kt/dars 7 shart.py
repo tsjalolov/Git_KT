7e073d2e0073495f609b6fc686aef1440aa92949
tsjalolov <tursunbekjalolov@gmail.com>
2024-03-18 14:15:15 +0500

    fail qo'shildi

---
+++ 1 2 3 KT amaliy 2 semestr/2 kt/dars 7 shart.py
@@ -0,0 +1,21 @@
+'''a = int(input('a = '))
+b = int(input('b = '))
+c = int(input('c = '))
+if a >= b and b >=c:
+    a = a * 2
+    b = b * 2
+    c = c * 2
+else:
+    if a < 0:
+        a = a * -1
+    if b < 0:
+        b = b * -1
+    if c < 0:
+        c = c * -1
+print('a =', a,'b =',b,'c =',c)'''
+x = int(input('x = '))
+y = int(input('y = '))
+if x > y:
+    z = x - y
+else:
+    z = y- x + 1
--- 1 KT 22  masalalar/aiogram_kt22/core/handlers/contact.py
+++ 1 KT 22  masalalar/aiogram_kt22/core/handlers/contact.py
@@ -6,6 +6,6 @@ async def get_true_contact(messange: Message, bot: Bot):
     if messange.contact.user_id == messange.from_user.id:
         await messange.answer(f"nomer to'g'ri {messange.contact.phone_number}")
     else:
-        await messange.answer(f"nomer no to'g'ri{messange.from_user.id}")
+        await messange.answer(f"nomer no to'g'ri {messange.from_user.full_name}")
 
 
