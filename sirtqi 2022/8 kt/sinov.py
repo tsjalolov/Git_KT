4ff63b8d773b0b65cd53f53563413e84e55d5d49
tsjalolov <tursunbekjalolov@gmail.com>
2024-05-17 15:01:52 +0500

    yangi o'zgarish1

--- sirtqi 2022/8 kt/sinov.py
+++ sirtqi 2022/8 kt/sinov.py
@@ -1,2 +1,15 @@
-a = 'salom'
-print(a)
+'''a = 4           #int
+b = 3.14        #float
+c = 'salom'     #str (string)
+d = True        #bool (boolean)
+print('a =',a, type(a))
+print('b =',b, type(b))
+print('c =',c, type(c))
+print('d =',d, type(d))
+x = a + b + c
+print(x)'''
+
+a = int(input('a ='))
+
+
+
---
+++ sirtqi 2022/9 kt/salom.py
@@ -0,0 +1,11 @@
+'''a = 5           #int
+b = 4.8         #float
+c = 'salom'     #str (string)
+t = True        #bool (bulean)
+print('a = ', a, type(a))
+print('b = ', b, type(b))
+print('c = ', c, type(c))
+print('t = ', t, type(t))'''
+
+a = int(input('a ='))
+print(a)
