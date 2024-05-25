2ba57459134096160ff6a9cb594ee76b0f77e45b
tsjalolov <tursunbekjalolov@gmail.com>
2024-05-25 17:35:33 +0500

    yangi o'zgarish342we

--- sirtqi 2022/8 kt/sinov.py
+++ sirtqi 2022/8 kt/sinov.py
@@ -9,7 +9,136 @@ print('d =',d, type(d))
 x = a + b + c
 print(x)'''
 
-a = int(input('a ='))
+'''a = int(input('a ='))
+b = int(input('b ='))
+x = a + b
+y = x / 2
+print('x =',x)
+print('y =',y)'''
+
+'''ism = input('ismingiz ->')
+fam = input('familiyangiz ->')
+print('assalomu aleykum ',fam,ism)'''
+
+
+
+'''a = int(input('a ='))
+b = int(input('b ='))
+if a > b:
+    print('a katta')
+else:
+    if a == b:
+        print('a teng b ga')
+    else:
+        print('b katta')'''
+
+'''
+m_parol = 'salom2'
+k_parol = input('parolingiz ->')
+if k_parol == m_parol:
+    print("parol to'g'ri")
+else:
+    print("parol noto'g'ri")'''
+
+'''
+i = 1
+while i < 6:
+    print(i)
+    i = i + 1'''
+'''
+i = 6
+while i > 0:
+    print("salom dunyo 1")
+    print("salom dunyo 2")
+    print("salom dunyo 3")
+    print("salom dunyo 4")
+    #i = i - 1
+
+'''
+
+
+
+'''
+while True:
+    print('salom')
+    son = int(input("sonni kiriting --> "))
+    for x in range(1,10 + 1,1):
+        p = son * x
+        print(son, '*',x,'=',p)
+    print('men',son,"karrani chiqardim")'''
+
+
+
+
+'''
+
+import time
+sanoq = 0
+while True:
+    s_pass = 'salom'
+    k_pass = input('parolni kiriting -->')    
+    if s_pass == k_pass:
+        print("parol to'g'ri  !!!!!")
+        sanoq = 0
+    else:
+        print("parol noto'g'ri")
+        sanoq += 1        
+        print('siz ',sanoq,'martta xato terdiz')
+        if sanoq == 5:
+            for i in range(1,5+1):
+                print(i,'- soniya')
+                time.sleep(1)            
+        if sanoq == 7:
+            for i in range(1,7+1):
+                print(i,'- soniya')
+                time.sleep(1)            
+        if sanoq == 9:
+            print('xayir    ')
+            break
+       
+'''
+
+import time
+while True:
+    for i in range(3):
+        print('sariq')
+        time.sleep(1)
+    print('qizil !!!!!')
+    time.sleep(5)
+    for i in range(3):
+        print('sariq')
+        time.sleep(1)
+    print('yashil  --->')
+    time.sleep(5)   
+    t = time.localtime()
+    hammasi = time.strftime("%H:%M:%S", t)
+    minut = time.strftime("%M", t)  #"%H:%M:%S"
+    print(hammasi)
+    print(minut)
+    print(type(int(minut)))
+    if int(minut) == 15:
+        print('sariq   maktab vaqti')
+        time.sleep(40)  #minit 15 ga teng bo'lganda 40 sekunt kutadi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- sirtqi 2022/9 kt/salom.py
+++ sirtqi 2022/9 kt/salom.py
@@ -7,5 +7,129 @@ print('b = ', b, type(b))
 print('c = ', c, type(c))
 print('t = ', t, type(t))'''
 
-a = int(input('a ='))
-print(a)
+
+
+
+
+'''a = int(input('a ='))
+b = int(input('b ='))
+x = a + b
+y = x/2
+print('x=',x)
+print('y=',y)'''
+
+'''ism = input('ismingizni kiriting ')
+fam = input('familiyangizni kiriting')
+print('assalomu aleykum ', fam, ism)
+'''
+
+
+'''a = int(input('a ='))
+b = int(input('b ='))
+if a > b:
+    print('a soni katta')
+else:
+    if a == b:
+        print('a = b ga')
+    else:
+        print('b soni katta')
+print(a,b)'''
+
+
+'''
+import hashlib
+h2 = hashlib.new('sha256')
+h = hashlib.new('sha256')
+h.update(b"salom2")
+#print(h.hexdigest())
+a = input('parolni kiriting  ')
+h.update(b'')
+if parol == a:
+    print("parol to'g'ri")
+else:
+    print('parol xato kiritildi')'''
+
+# for va while
+'''for i in range(2,51,2):
+    if i == 4:
+        print('>>>>>>>>>><<<<<<<<<')
+        continue
+    print('salom -->',i)'''
+
+
+'''son = int(input('sonni kiriting -> '))
+for i in range(1,11,1):
+    natija = son * i
+    print(son, '*',i,'=',natija)'''
+
+
+'''   
+for i in range(4):
+    m_parol = 'pass_salom'
+    k_parol = input('pass --> ')
+    if m_parol == k_parol:
+        print('uraaa')
+        break
+    else:
+        print('parol xato')
+else:
+    print('4 martta urindingiz uzur')'''
+
+
+
+import time
+sanoq = 0
+while True:
+    m_pass = 'salom'
+    k_pass = input('parolni kiriting -->')
+    #print('bizning parol    -->',m_pass)
+    #print('kiritilgan parol -->',k_pass)
+    if m_pass == k_pass:
+        print(">>>> to'g'ri <<<<")
+        sanoq = 0
+    else:
+        print("---- xato -----")
+        sanoq +=1
+        print('siz ',sanoq,'- martta xato terdingiz')
+        #time.sleep(5)
+        if sanoq == 5:
+            print('5 sekund kuting')
+            for i in range(1,5+1):
+                print(i, '- soniya')
+                time.sleep(1)           
+        if sanoq == 7:
+            print('7 sekund kuting')
+            for i in range(1,7+1):
+                print(i, '- soniya')
+                time.sleep(1)
+        if sanoq == 9:
+            print('xayir   !!!!')
+            break
+            
+            
+
+
+
+
+'''
+import time
+while True:
+    for i in range(1,4):
+        print('sariq')
+        time.sleep(1)
+    print('qizil   !!!!!')
+    time.sleep(5)
+    for i in range(1,4):
+        print('sariq')
+        time.sleep(1)
+    print('yashil ---->>>>>>>')
+    time.sleep(5)
+'''
+
+
+
+
+
+
+
+
