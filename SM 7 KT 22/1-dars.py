aef68c2fa8199a10e6079b2b0917d67e0f0c208c
tsjalolov <tursunbekjalolov@gmail.com>
2024-06-10 16:00:04 +0500

    sm 7gfht

---
+++ SM 7 KT 22/1-dars.py
@@ -0,0 +1,41 @@
+'''print('salom')
+a = 9 # int
+b = 3.5  # float
+s  = "salom" #str
+#c = a + b
+#d = c/2
+print(a,'-->',type(a))
+print(b,'-->',type(b))
+print(s,'-->',type(s))# odiljon faqat siz eshitmayapsiz
+'''
+'''a = int(input('a sonni kiriting -->'))
+b = int(input('b sonni kiriting -->'))
+print('a=',a)
+print('b=',b)
+'''
+'''
+ism = input('imingizni kiriting -->')
+familiya = input('familiyangizni kiriting -->')
+yil = int(input('yilingizni kiriting -->'))
+yosh = 2024 - yil
+print('assalomu aleykum',familiya,ism,'siz',yosh,'yoshdasiz')
+'''
+
+ism= input('ismingizni kiriting -->')
+familya= input('familaynigzni kiriting -->')
+yil=input('yilinizni kiritni-->')
+yosh=2024-yil
+print('assalomu alaykum',familya,ism,'siz',yosh,'yoshdasiz')
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ sirtqi 2022/9 kt/files.py
@@ -0,0 +1,34 @@
+
+
+f = open("sinov_file.txt", "r")
+print(f.read())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+
+
+
+
+f = open("demofile2.txt", "a")
+f.write("Now the file has more content!")
+f.close()'''
